6ae72f291413e3130f608f2ed7a62ce89f0a32c7
Thapelo kekana <thapelosimon@gmail.com>
2021-10-11 21:55:44 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,7 @@ axios.get('https://jsonplaceholder.typicode.com/users')
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello Axios</h1>
     </div>
   );
 }
